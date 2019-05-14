3f28f0423d470009306ca55a7b92d5fccff7e914
matijavavetic <47482084+matijavavetic@users.noreply.github.com>
2019-05-14 13:46:15 +0200

    Associative arrays, boolean, ternary operator

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 
+$task = [
+    'title' => 'Finish homework',
+    'due' => 'Today',
+    'assigned_to' => 'Jeffrey',
+    'completed' => false
+];
+
+
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <h1>Task For The Day</h1>
+
+    <ul>
+
+        <li>
+            <strong>Status: </strong> <?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
+        </li>
+        
+    </ul>
+
+</body>
+
+</html>
